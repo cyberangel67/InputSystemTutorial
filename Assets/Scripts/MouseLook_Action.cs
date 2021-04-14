50770228c8b755540f31398cfcfc91779aeb86dd
Andrew Scott <andrews@andyscott.id.au>
2021-04-15 06:41:23 +1000

    Update MouseLook_Action.cs

--- Assets/Scripts/MouseLook_Action.cs
+++ Assets/Scripts/MouseLook_Action.cs
@@ -19,7 +19,7 @@ public class MouseLook_Action : MonoBehaviour
         Cursor.visible = false;
 
         action = new InputAction(type: InputActionType.Value);
-        action.AddBinding("<Gamepad>/rightStick", processors: "ScaleVector2(x=0.3, y=0.3)");
+        action.AddBinding("<Gamepad>/rightStick", processors: "ScaleVector2(x=1, y=1)");
         action.AddBinding("<Mouse>/delta", processors: "ScaleVector2(x=0.5, y=0.5),ScaleVector2(x=0.1, y=0.1)");
         action.Enable();
     }
